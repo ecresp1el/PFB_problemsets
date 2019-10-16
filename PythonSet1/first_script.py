8ff74ed5f19a068d0fc1423f3cec0cf7e84d7918
info <info@info05.cshl.edu>
2019-10-16 11:31:32 -0400

    added one line for animals

--- PythonSet1/first_script.py
+++ PythonSet1/first_script.py
@@ -4,8 +4,11 @@
 name = "Emmanuel" 
 color = "Green" 
 activity = "Coding" 
+animal = "Chicken"
 
 #statements 
 print("My name:", name)
 print("My favorite color:", color)
 print("My faqvorite activity:", activity)
+print("My favorite animal:", animal)
+
