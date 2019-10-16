cf8d88d8d3da9916f1401e34541bdc9e4f61f5d0
info <info@info05.cshl.edu>
2019-10-16 11:24:47 -0400

    correct upload

---
+++ PythonSet1/first_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#define variables 
+name = "Emmanuel" 
+color = "Green" 
+activity = "Coding" 
+
+#statements 
+print("My name:", name)
+print("My favorite color:", color)
+print("My faqvorite activity:", activity)
