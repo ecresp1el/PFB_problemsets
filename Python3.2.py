a2f89092033488c93bbf3a91ea85c6b8a01acd36
info <info@info05.cshl.edu>
2019-10-17 22:19:55 -0400

    uploading both

---
+++ Python3.2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#save variable
+
+taxa = ['sapiens', 'erectus', 'neanderthalensis']
+print(taxa)
+print(taxa[1])
+print(type(taxa))
+print ( ' '.join(taxa))
+
+species = ' '.join(taxa)
+print(species)
+print(species[1])
+print(type(species))
+print(sorted(taxa))
+
+
